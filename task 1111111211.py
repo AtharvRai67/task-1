9dbd879dd63b06231d2ff32c76bd2943e7fafce3
AtharvRai67 <raiatharv6789@gmail.com>
2026-02-17 13:09:17 +0530

    Delete task 1111111211.py

--- task 1111111211.py
+++
@@ -1,9 +0,0 @@
-name = ("What is your name? Alex")
-age = ("how old are you? 21")
-hobby = ("what is ypur favourite hobby? coding")
-city = ("which city do you live in? Delhi")
-
-print(name)
-print(age)
-print(hobby)
-print(city)
